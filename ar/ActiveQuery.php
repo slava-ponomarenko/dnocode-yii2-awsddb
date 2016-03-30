04a23becc028b07426e1543085072c682a74bc9b
Vyacheslav Ponomarenko <slavikp92@gmail.com>
2016-03-30 18:01:46 +0300

    Fix compability bugs

--- ar/ActiveQuery.php
+++ ar/ActiveQuery.php
@@ -3,7 +3,6 @@
 namespace dnocode\awsddb\ar;
 
 use yii\db\ActiveQueryInterface;
-use yii\db\ActiveRecordInterface;
 
 
 /**
--- ddb/inputs/PutInput.php
+++ ddb/inputs/PutInput.php
@@ -43,7 +43,7 @@ class PutInput extends AWSInput {
      * @param $type  put | delete | add
      * @return array
      */
-    public function toArray($type){
+    public function toArray($type = NULL){
 
 
         $output=parent::toArray($type);
